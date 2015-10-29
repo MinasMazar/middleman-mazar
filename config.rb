3f439475f0ad58254ed9a19469073ba3dc30085d
MinasMazar <minasmazar@gmail.com>
2015-10-29 01:10:50 +0100

    First and last commit. (WIP)

---
+++ config.rb
@@ -0,0 +1,82 @@
+require "extensions/views"
+
+activate :views
+activate :directory_indexes
+activate :autoprefixer
+
+###
+# Blog settings
+###
+
+# Time.zone = "UTC"
+
+activate :blog do |blog|
+  # Name needed if your project hosts multiple blogs.
+  blog.name = "blog"
+  # This will add a prefix to all links, template references and source paths
+  blog.prefix = "blog"
+
+  blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "{year}-{month}-{day}-{title}.html"
+  blog.taglink = "tags/{tag}.html"
+  blog.layout = "blog_layout"
+  blog.summary_separator = /(READMORE)/
+  blog.summary_length = 250
+  blog.year_link = "{year}.html"
+  blog.month_link = "{year}/{month}.html"
+  blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".markdown"
+
+  blog.tag_template = "blog/tag_template.html"
+  blog.calendar_template = "blog/calendar_template.html"
+
+  # Enable pagination
+  blog.paginate = true
+  blog.per_page = 10
+  blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
+###
+# Compass
+###
+
+# Change Compass configuration
+# compass_config do |config|
+#   config.output_style = :compact
+# end
+set :relative_links, true
+set :css_dir, "assets/stylesheets"
+set :js_dir, "assets/javascripts"
+set :images_dir, "assets/images"
+set :fonts_dir, "assets/fonts"
+set :layout, "layouts/application"
+
+configure :development do
+ activate :livereload
+end
+
+configure :build do
+  # Relative assets needed to deploy to Github Pages
+  activate :relative_assets
+end
+
+activate :deploy do |deploy|
+  deploy.build_before = true
+  deploy.method = :git
+end
+
+helpers do
+  def nav_link(link_text, page_url, options = {})
+    options[:class] ||= ""
+    if current_page.url.length > 1
+      current_url = current_page.url.chop
+    else
+      current_url = current_page.url
+    end
+    options[:class] << " active" if page_url == current_url
+    link_to(link_text, page_url, options)
+  end
+end
